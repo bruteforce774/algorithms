e69e61166d8f14c0f214bb0b5b2339610b6e93a5
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-04 17:09:36 +0100

    add insertion sort

--- code/utils.h
+++ code/utils.h
@@ -1,8 +1,30 @@
 #pragma once
 
-// function prototype for insertion_sort
-void insertion_sort(int arr[], int n);
+// Insertion Sort - sorts an array in ascending order
+// arr: array to be sorted
+// n: number of elements in the array
+void insertion_sort(int arr[], int n) {
+	for (int i = 1; i < n; i++) {
+		int key = arr[i];
+		// Insert arr[i] into the sorted subarray arr[0:i-1]
+		int j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
 
 void insertion_sort(double arr[], int n) {
-	// implementation for insertion_sort
+	for (int i = 1; i < n; i++) {
+		double key = arr[i];
+		// Insert arr[i] into the sorted subarray arr[0:i-1]
+		int j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
 }
