fafe40286079814ed6db780aab6c94462de355c7
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-04 17:11:47 +0100

    use insertion sort in main

--- code/main.cpp
+++ code/main.cpp
@@ -5,4 +5,13 @@ using std::cout, std::endl;
 
 int main() {
     int a[] = {5, 4, 2, 6, 1, 3};
+
+    insertion_sort(a, 6);
+
+    std::cout << "Sorted integer array: ";
+    for (int i = 0; i < 6; ++i) {
+        std::cout << a[i] << " ";
+    }
+    
+    std::cout << std::endl;
 }
