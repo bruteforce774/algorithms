64a5f442ed68fee8cfff2ad9add5b2390097bdee
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-04 17:00:55 +0100

    first commit

---
+++ code/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "utils.h"
+
+using std::cout, std::endl;
+
+int main() {
+    int a[] = {5, 4, 2, 6, 1, 3};
+}
---
+++ code/utils.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// function prototype for insertionSort
+void insertion_sort(int arr[], int n);
+
+void insertion_sort(double arr[], int n) {
+	// implementation for insertionSort
+}
